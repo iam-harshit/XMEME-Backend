a0f9d757280495bd0cc192875ffa212ad79c8e55
CrioUser <crio-user@criodo.com>
2022-11-28 11:41:34 +0530

    ME_BUILDOUT_XMEME_JAVA_MODULE_BASIC

--- src/main/java/com/crio/starter/Swagger/SwaggerConfiguration.java
+++ src/main/java/com/crio/starter/Swagger/SwaggerConfiguration.java
@@ -1,18 +1,18 @@
-package com.crio.starter.Swagger;
+// package com.crio.starter.Swagger;
 
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import springfox.documentation.spring.web.plugins.Docket;
+// import springfox.documentation.swagger2.annotations.EnableSwagger2;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableSwagger2
-public class SwaggerConfiguration {
+// @Configuration
+// @EnableSwagger2
+// public class SwaggerConfiguration {
     
-    @Bean
-    public Docket getBean(){
-        return null;
+//     @Bean
+//     public Docket getBean(){
+//         return null;
 
-    }
+//     }
 
-}
+// }
